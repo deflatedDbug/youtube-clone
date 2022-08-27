5e4fa6eca6d7c074700392e9746ee3fe4a5c6b10
deflatedDbug <101749459+deflatedDbug@users.noreply.github.com>
2022-08-27 15:56:13 -0400

    Adding components & routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-function App() {
-  return <div className="App">Youtube Clone</div>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  ChannelDetail,
+  Feed,
+  Navbar,
+  SearchFeed,
+  VideoDetail,
+} from "./components";
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" exact element={<VideoDetail />} />
+        <Route path="/channel/:id" exact element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
 export default App;
